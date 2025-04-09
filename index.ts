473a99905c2481cbc0a39a840e10063f31c35571
NICKYTOMBA88 <mauinv88@gmail.com>
2025-04-09 01:31:07 -0300

    creacion de mi primer usuario desde mi sistema en ts

--- index.ts
+++ index.ts
@@ -12,20 +12,20 @@ connectDB()
 
 
 //Interface para typescript para mi sistema
-interface User extends Document {
+interface UserIterface extends Document {
   name: string
   email: string
   city: string
-  role: "user" | "admin"
+  role?: "user" | "admin"
   age: number
 }
 
 //Esquema o molde para los datos una vez que son enviados a la base de datos
 // Un esquema espera a un objeto para definir todas las propiedades
 // valida las propiedades antes de guardarlas en la base de datos
-const userSchema: Schema = new Schema<User>({
+const userSchema: Schema = new Schema<UserIterface>({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, match: /^[._%+-]+@[a-zA-Z0-9.-]+\.{2,}$/, },
+  email: { type: String, required: true, unique: true, match: /^[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ },
   city: { type: String, required: true },
   role: {
     type: String, required: true, enum: ["user", "admin"], default: "user"
@@ -34,3 +34,24 @@ const userSchema: Schema = new Schema<User>({
 }, { timestamps: false, versionKey: false })
 
 userSchema.set("strict", true) //esto es para que no acepte cualquier propiedad que se agregue
+
+const User = mongoose.model<UserIterface>("User", userSchema)
+
+// Es una funcion asyncrona por que esto va a la base de datos e intenta hacer algo en este caso crear un usuario
+const createUser = async () => {
+  try {
+    const user: UserIterface = new User({
+      name: "Nicolas",
+      email: "nicolas@gmail.com",
+      city: "Mendoza",
+      age: 22,
+      role: "user"
+    })
+
+    await user.save() //Quiere decir inserOne() en mongodb
+    console.log("Usuario registrado")
+  } catch (error) {
+    console.log("error al registrar el usuario")
+  }
+}
+createUser()
