5c657379d17db67d7c8d0ec77af181e78f239c25
NICKYTOMBA88 <mauinv88@gmail.com>
2025-04-14 16:47:22 -0300

    Cambio la entidad de usuario a neumaticos con el CRUD completo

--- index.ts
+++ index.ts
@@ -26,8 +26,8 @@ const createTire = async () => {
   try {
     const tire = new Tire({
       width: 185,
-      profile: 65,
-      rolled: 14,
+      profile: 60,
+      rolled: 15,
     });
 
     await tire.save();
@@ -42,22 +42,25 @@ const getTires = async () => {
   try {
     const tires = await Tire.find();
     tires.forEach((t) => {
-      console.log(`${t.width}/${t.profile}-${t.rolled}`);
+      console.log(`ID: ${t._id} - ${t.width}/${t.profile}-${t.rolled}`,);
     });
   } catch (error) {
     console.log("Error al obtener neumáticos:", error);
   }
 };
 
-// const getUserByName = async (name: string) => {
-//   try {
-//     const users = await getUser();
-//     const user = await User.findOne({ name: { $regex: name, $options: "i" } })
-//     console.log(user)
-//   } catch (error) {
-//     console.log("error al mostrar usuarios", error)
-//   }
-// }
+const updateTire = async (id: string, body: Partial<TireSize>) => {
+  try {
+    const updatedTire = await Tire.findByIdAndUpdate(id, body, { new: true });
+    if (!updatedTire) {
+      console.log("No se encuentra el neumático");
+    } else {
+      console.log("Neumático actualizado:", updatedTire);
+    }
+  } catch (error) {
+    console.log("Error al actualizar neumático:", error);
+  }
+};
 
 
 // const updateUser = async (id: string, body: object) => {
@@ -75,19 +78,24 @@ const getTires = async () => {
 // }
 
 
-// // deleteUser("67f67d6475f25f426a0a22d6")
-// const deleteUser = async (id: string) => {
-//   const deletedUser = await User.findByIdAndDelete(id)
-//   try {
-//     if (!deletedUser) {
-//       console.log("no se encuentra el usuario");
-//     } else {
-//       console.log(deletedUser, "Usuario borrado");
-//     }
-//   } catch (error) {
-//     console.log("no se borro el usuario", error)
-//   }
 
-// };
+const deleteTire = async (id: string) => {
+  const deletedTire = await Tire.findByIdAndDelete(id)
+  try {
+    if (!deletedTire) {
+      console.log("no se encuentra el neumático");
+    } else {
+      console.log(deletedTire, "Neumatico borrado");
+    }
+  } catch (error) {
+    console.log("no se borro el neumatico", error)
+  }
 
-getTires()
+};
+
+// CRUD
+// createTire()
+// getTires()
+// updateTire()
+// updateTire("aquí_el_id", { profile: 65 });
+// deleteTire()
